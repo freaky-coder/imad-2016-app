08e7e348f3296769e2112fb80bffda3b0002119b
Ayush Kapoor <kapoor.ayush1901@gmail.com>
2016-10-25 21:57:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/article-three',function(req,res){
   res.sendFile(path.join(__dirname, 'ui', 'article-three.html')); 
 });
 
-
+<!--
 app.get('/article-two',function(req,res){
   res.send('Article two Url is requested and will be servbed here.');  
 });
@@ -29,6 +29,7 @@ app.get('/article-two',function(req,res){
 app.get('/article-three',function(req,res){
   res.send('Article three Url is requested and will be servbed here.');  
 });
+-->
 app.get('/article-four',function(req,res){
   res.send('Article four Url is requested and will be servbed here.');  
 });
