a11e2414aecd6ddf092e5dd7f1b83ccbd46d5a28
Ayush Kapoor <kapoor.ayush1901@gmail.com>
2016-10-25 19:51:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-var express = require('express'); // Create web server
-var morgan = require('morgan'); // Output Logs
+var express = require('express'); 
+var morgan = require('morgan'); 
 var path = require('path');
 
 var app = express();
