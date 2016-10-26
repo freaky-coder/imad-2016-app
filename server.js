21b8acae56ea49c812150a37a1e8404d0a58cbf9
Ayush Kapoor <kapoor.ayush1901@gmail.com>
2016-10-26 19:33:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,6 +20,45 @@ var articleOne={
                                 </p>`
 };
 
+var htmlTemplate= `
+    <html>
+        <head>
+            <title>
+                Article-One | Freaky Coder
+            </title>
+            <meta name="viewport" content="width-device-width, initial-scale=1"/>
+        <link href="/ui/style.css" rel="stylesheet" />
+            </head>
+            
+                <body>
+                    <div class="container">
+                    <div>
+                        <a href="/">Home</a>
+                        </div>
+                        <hr/>
+                        <h3>
+                            Article One
+                            </h3>
+                            <div>
+                                Oct 25, 2016
+                            </div>
+                            <div>
+                                <p>
+                                    This is the content of first article. This is the content of first article
+                                    </p>
+                                    <p>
+                                    This is the content of first article. This is the content of first article
+                                    </p>
+                                    <p>
+                                     This is the content of first article. This is the content of first article
+                                    </p>
+                                </div>
+                      </div>
+                    </body>
+    </html>
+`;
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
