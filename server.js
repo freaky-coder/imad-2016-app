2990a70dfb374fc839fd9a7e96395b64f9a8f887
Ayush Kapoor <kapoor.ayush1901@gmail.com>
2016-11-02 22:08:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -111,9 +111,6 @@ app.get('/ui/madi.png', function (req, res) {
   
 });
 
-app.get('/ui/main.js', function (req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
 
 app.get('/ui/main.js' , function(req,res){
    res.sendFile(path.join__dirname, 'ui' , 'main.js'); 
