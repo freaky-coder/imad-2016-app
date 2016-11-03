7858694f2c737f2c0bd68e6a8781308521a4408e
Ayush Kapoor <kapoor.ayush1901@gmail.com>
2016-11-03 21:32:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 // Change the text of main HTML file
 
-var element = document.getElementById("element");
+var element = document.getElementById('element');
 
 element.innerHTML = 'New Value';
