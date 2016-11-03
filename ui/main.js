34c6a81520613cd250db7b28f53b9aed87ae554d
Ayush Kapoor <kapoor.ayush1901@gmail.com>
2016-11-03 20:24:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 // Change the tesxt of main file
 
-var element= document.getElementById("element");
+var element= document.getElementById('element');
 
 element.innerHTML = 'New Value';
